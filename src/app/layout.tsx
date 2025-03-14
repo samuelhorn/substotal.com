85aba66359c2958bbc4b1707b3c4a2fbe20047bb
Samuel Horn af Rantzien <samuel@samuelhorn.com>
2025-03-14 17:28:07 +0100

    refactor: landing page ssr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className="scroll-smooth">
       <body className={`${gabarito.className} antialiased`}>
         <ThemeProvider
           attribute="class"
