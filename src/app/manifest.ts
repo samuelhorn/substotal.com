4ac99e24e8837afe37f8b0bda67184379c30fc11
Samuel Horn af Rantzien <samuel@samuelhorn.com>
2025-03-20 23:28:14 +0100

    refactor: manifest updates

---
+++ src/app/manifest.ts
@@ -0,0 +1,27 @@
+import type { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+    return {
+        name: 'Substotal - Subscription Tracker',
+        short_name: 'substotal',
+        description: 'A simple app to track your recurring subscriptions',
+        start_url: '/',
+        display: 'fullscreen',
+        background_color: '#1e1812',
+        theme_color: '#F7EBAF',
+        icons: [
+            {
+                src: "/web-app-manifest-192x192.png",
+                sizes: "192x192",
+                type: "image/png",
+                purpose: "maskable"
+            },
+            {
+                src: "/web-app-manifest-512x512.png",
+                sizes: "512x512",
+                type: "image/png",
+                purpose: "maskable"
+            }
+        ],
+    }
+}
